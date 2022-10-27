c7e404b488a315378b4afc7d74b5ce5eaa5092dc
Mariane <hounsounorma@gmail.com>
2022-10-27 06:49:32 +0100

    Challenge_js_2

--- challenge.js
+++ challenge.js
@@ -69,22 +69,49 @@ const New = document.getElementById("new");
 const Contact = document.getElementById("contact");
 const Remove = document.getElementById("remove");
 
+
+// ============================clique du bouton add======================
 function msg() {
   document.getElementById("remove").innerHTML = "";
   let nom = document.getElementById("Title").value;
   let prenom = document.getElementById("Author").value;
-  document.getElementById("remove").innerHTML = `"${nom}"` + " by " + prenom;
+
+  //insertion de div
+  let newDiv = document.createElement("div");
+  newDiv.id = "nouveau";
+
+  // insertion du texte
+  let newPara = document.createElement("p");
+  newPara.id = "nouveauPara";
+  let texte = document.createTextNode(`" ${nom} "` + " by " + prenom);
+  newPara.appendChild(texte);
+
+  // insertion de bouton
+  let newButton = document.createElement("button");
+  newButton.id = "btn";
+  let texte1 = document.createTextNode("Remove");
+  newButton.appendChild(texte1);
+
   if (New.style.display == "block") {
+    document.getElementById("active1").classList.add("active");
+    document.getElementById("active2").classList.remove("active");
+    document.getElementById("remove").classList.add("remove1");
+    document.getElementById("remove").appendChild(newDiv);
+    document.getElementById("nouveau").appendChild(newPara);
+    document.getElementById("nouveau").appendChild(newButton);
     New.style.display = "none";
     List.style.display = "block";
     Remove.style.display = "block";
   }
-  else if(    Remove.style.display == "block"){
-    New.style.display = "none";
-    List.style.display = "block";
-    Remove.style.display = "none";
-  }
+  newButton.addEventListener("click", () => {
+    if (Remove.style.display == "block") {
+      document.getElementById("active1").classList.add("active");
+      Remove.style.display = "none";
+    }
+  });
 }
+
+
 
 function fct1() {
   if (New.style.display == "block") {
